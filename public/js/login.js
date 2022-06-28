ccd59d3617ef573fa137241509d4f5ad0392cb9e
Mikekuney <96913841+Mikekuney@users.noreply.github.com>
2022-06-27 23:02:07 -0400

    adjusted public/js files

--- public/js/login.js
+++ public/js/login.js
@@ -1,27 +1,25 @@
-async function loginFormHandler(event) {
+const loginFormHandler = async function(event) {
   event.preventDefault();
 
-  const email = document.querySelector('#email-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
+  const usernameEl = document.querySelector('#username-input-login');
+  const passwordEl = document.querySelector('#password-input-login').value.trim();
 
-  if (email && password) {
-    const response = await fetch('/api/user/login', {
-      method: 'POST',
-      body: JSON.stringify({
-        email,
-        password
-      }),
-      headers: { 'Content-Type': 'application/json' },
-    });
+  const response = await fetch('/api/user/login', {
+    method: 'POST',
+    body: JSON.stringify({
+      username: usernameEl.value,
+      password: passwordEl.value,
+    }),
+    headers: { 'Content-Type': 'application/json' },
+  });
 
-    if (response.ok) {
-      document.location.replace('/dashboard/');
-    } else {
-      alert('Failed to login');
-    }
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert('Failed to login');
   }
 };
 
 document
-  .querySelector('.login-form')
+  .querySelector('#login-form')
   .addEventListener('submit', loginFormHandler);
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,26 +1,24 @@
 async function signupFormHandler(event) {
   event.preventDefault(); 
 
-  const username = document.querySelector('#username-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
+  const usernameEl = document.querySelector('#username-input-signup');
+  // const email = document.querySelector('#email-signup').value.trim();
+  const passwordEl = document.querySelector('#password-input-signup');
 
-  if (username && email && password) {
-    const response = await fetch('/api/users', {
-      method: 'POST',
-      body: JSON.stringify({
-        username,
-        email,
-        password 
-      }),
-      headers: { 'Content-Type': 'application/json' },
-    });
+  const response = await fetch('/api/user', {
+    method: 'POST',
+    body: JSON.stringify({
+      username: usernameEl.value,
+      // email,
+      password: passwordEl.value,
+    }),
+    headers: { 'Content-Type': 'application/json' },
+  });
 
-    if (response.ok) {
-      document.location.replace('/dashboard/');
-    } else {
-      alert('Failed to sign up');
-    }
+  if (response.ok) {
+    document.location.replace('/dashboard/');
+  } else {
+    alert('Failed to sign up');
   }
 };
 
